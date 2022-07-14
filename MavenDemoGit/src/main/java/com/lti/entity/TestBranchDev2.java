c5181928ed012ce9b8762fb2bcdfb2ad2e5739fa
NamitaMohanty <namitamohanty692@gmail.com>
2022-07-14 14:35:14 +0530

    sec branch

--- MavenDemoGit/src/main/java/com/lti/entity/TestBranchDev2.java
+++ MavenDemoGit/src/main/java/com/lti/entity/TestBranchDev2.java
@@ -3,5 +3,6 @@ package com.lti.entity;
 public class TestBranchDev2 {
     public static void main(String[] args) {
 		System.out.println(" test decbranch2");
+		System.out.println(" test decbranch2");
 	}
 }
